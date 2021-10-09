1d95113cc8795ec3fbad1a33721437cdde4b8955
ATV-byte <57212902+ATV-byte@users.noreply.github.com>
2021-10-09 17:25:55 +0300

    afisez numarul de vocale si consoane

--- session2/ex29.py
+++ session2/ex29.py
@@ -9,3 +9,15 @@
         2 (pentru vocale)
         4 (pentru consoane)
 """
+x = input()
+x = x.lower()
+vocals = 0
+consonant = 0
+for el in x:
+    if el == 'a' or el == 'e' or el == 'i' or el == 'o' or el == 'u':
+        vocals = vocals + 1
+    else:
+        consonant = consonant + 1
+
+print(vocals)
+print(consonant)
