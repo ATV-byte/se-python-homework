25922d7f4a2d9f3e5abd5f0afc43f9c2caa0bd6e
ATV-byte <57212902+ATV-byte@users.noreply.github.com>
2021-10-09 17:19:55 +0300

    generez stringuri random cu lungimea data

--- session2/ex27.py
+++ session2/ex27.py
@@ -7,3 +7,11 @@
         Veti primi: 5
         Veti printa: 'ashdj' (poate fi orice alt string)
 """
+import string
+import random
+letters = string.ascii_letters
+x = int(input())
+y = ''
+for i in range(x):
+    y = y + random.choice(letters)
+print(y)
