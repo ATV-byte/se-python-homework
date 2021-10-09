76400af47ea9ff91d4103da9bbd12d8c68c5ab98
ATV-byte <57212902+ATV-byte@users.noreply.github.com>
2021-10-09 17:35:08 +0300

    verific daca secventa este corecta sau nu

--- session2/ex30.py
+++ session2/ex30.py
@@ -10,3 +10,33 @@
         Veti primi: '(()]'
         Veti printa: False
 """
+x = input()
+nb_round_open = 0
+nb_round_close = 0
+nb_square_open = 0
+nb_square_close = 0
+nb_curly_open = 0
+nb_curly_close = 0
+for el in x:
+    if el == '(':
+        nb_round_open = nb_round_open + 1
+    if el == ')':
+        nb_round_close = nb_round_close + 1
+    if el == '[':
+        nb_square_open = nb_square_open + 1
+    if el == ']':
+        nb_square_close = nb_square_close + 1
+    if el == '{':
+        nb_curly_open = nb_curly_open + 1
+    if el == '}':
+        nb_curly_close = nb_curly_close + 1
+
+if(nb_round_open == nb_round_close and nb_square_open == nb_square_open and nb_curly_open == nb_curly_close):
+    vf = True
+else:
+    vf = False
+
+print(vf)
+
+
+
