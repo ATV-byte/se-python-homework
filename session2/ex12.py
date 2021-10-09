a89547689b026e368f1e3881d17b074415a22120
ATV-byte <57212902+ATV-byte@users.noreply.github.com>
2021-10-09 15:18:16 +0300

    am realizat programul

--- session2/ex12.py
+++ session2/ex12.py
@@ -8,3 +8,14 @@
         Veti primi 6, veti afisa [1, 2, 3, 4, 5]
         Veti primi 5, veti afisa [1, 4, 9, 16]
 """
+
+x = int(input())
+i = 1
+l = []
+while i < x:
+    if x % 2 == 0:
+        l.append(i)
+    else:
+        l.append(i*i)
+    i = i + 1
+print(l)
