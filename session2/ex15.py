b88d7f61a38895ba58fb703696305c113d799708
ATV-byte <57212902+ATV-byte@users.noreply.github.com>
2021-10-09 15:30:38 +0300

    afisez numarul vocalelor

--- session2/ex15.py
+++ session2/ex15.py
@@ -6,3 +6,12 @@
         Veti primi: 'cmi'
         Veti printa: 1
 """
+
+x = input()
+x = x.lower()
+vocals = 0
+for el in x:
+    if el == 'a' or el == 'e' or el == 'i' or el == 'o' or el == 'u':
+        vocals = vocals + 1
+
+print(vocals)
